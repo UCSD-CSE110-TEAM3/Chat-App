9dca6ca482d2283e724374699fb13be6192e2090
JT <rospacebrown@gmail.com>
2013-11-06 19:49:22 -0800

    Basic Test setUp and Tear down, with protected access if we want to test other classes

--- ChatClient/src/test/java/edu/ucsd/cse110/client/ChatClientTest.java
+++ ChatClient/src/test/java/edu/ucsd/cse110/client/ChatClientTest.java
@@ -7,22 +7,43 @@ package edu.ucsd.cse110.client;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 /**
- * Tests the ChatClient classs.
+ * Tests the ChatClient class.
  *
  */
 public class ChatClientTest {
+	
+	protected ChatClient testClientSender;
+	protected ChatClient testClientReceiver1;
+	protected ChatClient testClientReceiver2;
+	
+	@Before
+	private void setUp(){
+		testClientSender = new ChatClient(null, null, "sender", "test");
+		testClientReceiver1 = new ChatClient(null, null, "receiver1", "test");
+		testClientReceiver2 = new ChatClient(null, null, "receiver2", "test");
+	}
+	
+	@After
+	private void tearDown(){
+		testClientSender = null;
+		testClientReceiver1 = null;
+		testClientReceiver2 = null;
+	}
 
 	@Test
 	public void testSend() {
-		try {
-			ChatClient myClient = new ChatClient( null, null );
-		} catch (Exception e ) {
-			fail( "Constructor threw an exception ");
-		}		
+		try{
+			testClientSender.logon();
+		}
+		catch(Exception e){
+			System.err.println("could not log in");
+		}
 	}
 
 }
