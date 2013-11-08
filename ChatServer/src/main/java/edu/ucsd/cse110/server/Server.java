c1526f5843cab45f349408d6da24a18efba8bbb5
Mimi <nqnliu@hotmail.com>
2013-11-07 20:40:15 -0800

    Cleaned up.

--- ChatServer/src/main/java/edu/ucsd/cse110/server/Server.java
+++ ChatServer/src/main/java/edu/ucsd/cse110/server/Server.java
@@ -1,6 +1,13 @@
 package edu.ucsd.cse110.server;
 
+
 import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader; 
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -14,6 +21,7 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 
 public class Server{
+	
 	JmsTemplate template;
 	private HashMap<String, Destination> online;
 	private HashMap<String, String> accounts; //the accounts that is loaded to memory for now.
@@ -23,7 +31,68 @@ public class Server{
 		template = new JmsTemplate(factory);
 		//template = jmsTemplate;
 		online = new HashMap<String, Destination>();
+		
+		accounts = new HashMap<String, String>();
+		
+		try {
+			load_accounts();
+		} catch (IOException e) {
+			System.out.println("accounts not loaded");
+		}
+		
 	}
+	
+	//loads account info from disk to hashmap
+	public void load_accounts() throws IOException{
+		
+		FileReader f0 = null;
+		f0 = new FileReader("user_accounts.txt");
+		
+		BufferedReader br = new BufferedReader(f0);
+		String line;
+		while ((line = br.readLine()) != null) {
+			List<String> pair = Arrays.asList(line.split(":"));
+			accounts.put(pair.get(0), pair.get(1));
+		}
+		br.close();
+		f0.close();
+			 
+
+	}
+	//dumps pairs in hashmap to disk
+	public void save_accounts() throws IOException {
+		
+		 FileWriter f0 = null;
+		 f0 = new FileWriter("user_accounts.txt");
+		 
+		 String nl = System.getProperty("line.separator"); // '/n' not very portable
+		 
+		 //Dumping all key, values into file , line by line
+		 //dumps it in this format
+			 // user01:password01
+			 // user02:password02
+			 
+			 //System.out.println("key : " + key)
+			 //System.out.println("value : " + accounts.get(key));
+			 
+		 for (String key: accounts.keySet()) {
+			 f0.write( key +":"+ accounts.get(key)+nl);
+		 }
+		 
+         	f0.close();
+		 
+		 
+	}
+	//checks from hashmap
+	public boolean user_exists(String input) {
+		
+		if (this.accounts.get(input)!=null) {
+			return true;	
+		} 	
+		return false;	
+		
+	}
+	
 	//puts in hashmap and writes to disk
 	public boolean make_account(String user, String password){
 		
@@ -42,8 +111,23 @@ public class Server{
 	
 	
 	
+	
+	
 	public void setSender( JmsTemplate jmsTemplate ) {
 		this.template = jmsTemplate;
+	}
+	
+	//logs in by checking if account exsists and password matches in hashmap
+	public boolean valid_login(String user, String password) {
+		
+		if (password == null) {
+			return false;
+		}
+		if (this.accounts.get(user)==password) {
+			return true;
+		}
+		return false; 
+		
 	}
 	
 	//logs in by checking if account exsists and password matches in hashmap
